64f0a88d91168055fb7dc3428c355c1e4d248490
Lee Sing Yuan <slee0163@student.monash.edu>
2025-01-07 01:27:59 +0800

    modified the vite.config.js

--- viteTest1/vite.config.js
+++ viteTest1/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/testVite1/",
+  plugins: [
+    react(),
+    ghPages(),],
   server: {
     port: 3000, // Replace 3000 with your desired port
   },
